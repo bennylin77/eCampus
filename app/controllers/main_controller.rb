7328d31041f47f15f087d87ea603db91079f103a
bennylin77 <bennylin77@gmail.com>
2014-11-11 10:43:04 +0800

    First commit

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,12 @@
+class MainController < ApplicationController
+  def index
+  end
+  
+  def courseSearch
+    unless params[:term].blank?
+      @result =RestClient.post 'http://140.113.8.134/E35/Search/Index', QueryString: params[:term]    
+      @result = @result.force_encoding('utf-8').encode
+      @result=JSON.parse(@result)  
+    end
+  end
+end

17d89b1b8fcdfd5afacf50f277a3edf9564674e6
tychien <t6847kimo@gmail.com>
2015-01-06 00:04:50 +0800

    route merge?

--- app/controllers/forum_controller.rb
+++ app/controllers/forum_controller.rb
@@ -1,9 +1,20 @@
 class ForumController < ApplicationController
-  before_action :set_course, only: [:index]
+  before_action :set_course, only: [:index, :new]
 	
   def index
 	@res=postRequest(apiURL+"Forum/ListbyCourse",{:courseId=>"00000000-0000-0000-0000-000000000000"})
 	
+  end
+  def new
+	
+  end
+  def create
+	payload={
+		:accountId=>currentUser.id,
+		:courseId=>@course_id,
+		:title=>params[:title]
+	}
+	@res=postRequest(apiURL+"Forum/CreateForum",payload)
   end
   
 private
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   resources :courses do
 	get "forum/index"
 	get "forum/new"
+	post "forum/create"
   end
   
   
